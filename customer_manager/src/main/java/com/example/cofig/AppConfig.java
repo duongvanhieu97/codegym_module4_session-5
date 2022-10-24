ca9f2945f5f5bd95987ec48e806b39fcc681c944
Duonghieu97st <Duonghieu97st@gmail.com>
2022-10-24 13:53:20 +0700

    update

---
+++ customer_manager/src/main/java/com/example/cofig/AppConfig.java
@@ -0,0 +1,35 @@
+package com.example.cofig;
+
+import com.example.service.CustomerService;
+import com.example.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example")
+public class AppConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/templates/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+    @Bean
+    public CustomerService customerService() {
+        return new SimpleCustomerServiceImpl();
+    }
+}
